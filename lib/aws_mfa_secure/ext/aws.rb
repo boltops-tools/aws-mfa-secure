2e539725bd5260a1835bfefbddd620806e11787a
Tung Nguyen <tongueroo@gmail.com>
2019-11-09 16:41:30 +0000

    add aws extension

---
+++ lib/aws_mfa_secure/ext/aws.rb
@@ -0,0 +1,20 @@
+require "aws-sdk-core"
+
+module AwsMfaCredentials
+  def initialize(*)
+    credentials = AwsMfaSecure::Credentials.instance
+    if credentials.set?
+      @access_key_id = credentials.access_key_id
+      @secret_access_key = credentials.secret_access_key
+      @session_token = credentials.session_token
+    else
+      super
+    end
+  end
+end
+
+module Aws
+  class Credentials
+    prepend AwsMfaCredentials
+  end
+end
